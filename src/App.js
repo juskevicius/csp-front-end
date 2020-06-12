ba4a8c8e82246b187dff4d5f00c97628e641e30e
mantas_juskevicius <mantas.juskevicius@zenitech.co.uk>
2020-06-12 23:18:57 +0300

    Moved greetings to an array and mapped them to buttons

--- src/App.js
+++ src/App.js
@@ -5,24 +5,28 @@ import { Response } from "./response";
 const App = () => {
   const [greeting, setGreeting] = useState("");
 
+  const greetings = ["Hello", "Goodbye", "Salutations"];
+
   return (
     <>
-      <div className={styles.title}>Click on a greeting to get a proper response to it</div>
+      <div className={styles.title}>
+        Click on a greeting to get a proper response to it
+      </div>
       <div className={styles.btnContainer}>
         <span className={styles.btnWrapper}>
-          <button onClick={() => setGreeting("hello")} className={styles.greetBtn}>
-            Hello
-          </button>
-          <button onClick={() => setGreeting("goodbye")} className={styles.greetBtn}>
-            Goodbye
-          </button>
-          <button onClick={() => setGreeting("salutations")} className={styles.greetBtn}>
-            Salutations
-          </button>
+          {greetings.map((greet) => (
+            <button
+              key={greet}
+              onClick={() => setGreeting(greet.toLowerCase())}
+              className={styles.greetBtn}
+            >
+              {greet}
+            </button>
+          ))}
         </span>
       </div>
       <div className={styles.title}>Response to the greeting:</div>
-      <Response greeting={greeting}/>
+      <Response greeting={greeting} />
     </>
   );
 };
